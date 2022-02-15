e17d75a314969d63507524dad6fb891ad22c9cee
andre9653 <andresena9653@gmail.com>
2022-02-15 19:31:21 -0300

    Adiciona algumas configurações de teste

--- src/server.js
+++ src/server.js
@@ -8,4 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
 app.listen(PORT, () => console.log(`Online na porta ${PORT}`));
+
+module.exports = app;
