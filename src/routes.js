c3e3a0768462eda1775416ec547bf1330a3c7c8b
andre9653 <andresena9653@gmail.com>
2022-02-16 16:13:42 -0300

    Testando funcionamento

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
 const routes = express.Router();
-
+routes.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 require('dotenv').config();
+const cors = require('cors');
 
 const { PORT } = process.env;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
-});
 
 app.listen(PORT, () => console.log(`Online na porta ${PORT}`));
 
